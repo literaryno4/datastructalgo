c25d1ac95837f739127dffe0adf48087938b1ea9
literaryno4 <m18260037123@163.com>
2022-07-07 11:37:27 +0800

    update lockfree_stack.hpp

--- lockfree_stack.hpp
+++ lockfree_stack.hpp
@@ -39,6 +39,9 @@ public:
         newNode.ptr = new Node(std::forward<T&&>(data));
         newNode.externalCount = 1;
         newNode.ptr->next = head.load(std::memory_order_relaxed);
+        
+        // before make head be newNode, we must make sure head is now point to next of newNode(compare and exchange). 
+        // If not, make head point to next of newNode and compare and exchange again.
         while (!head.compare_exchange_weak(newNode.ptr->next, newNode, std::memory_order_release, std::memory_order_relaxed));
     }
 
@@ -50,14 +53,20 @@ public:
             if (!ptr) {
                 return std::shared_ptr<T>();
             }
-            if (head.compare_exchange_strong(oldHead, ptr->next)) {
+            if (head.compare_exchange_strong(oldHead, ptr->next, std::memory_order_relaxed)) {
                 std::shared_ptr<T> res;
                 res.swap(ptr->data);
+
+                // here minus 2 because externalCount inited to be 1 in push function, and add another 1 when loaded.
                 int const countIncrease = oldHead.externalCount - 2;
+
+                // add countIncrease to internalCount because CountedNodePtr is no longer existing, but node may not deleted
+                // unless no other thread is reading this node. Anyway, we must add all external reference count to internal.
                 if (ptr->internalCount.fetch_add(countIncrease, std::memory_order_release) == -countIncrease ) {
                     delete ptr;
                 }
                 return res;
+            // if another thread popped, check this node whether is only 1 reference count. If it is, delete this node.
             } else if (ptr->internalCount.fetch_add(-1, std::memory_order_relaxed) == 1) {
                 ptr->internalCount.load(std::memory_order_acquire);
                 delete ptr;
